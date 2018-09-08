1db50f4beb6e3d2c5c146f9cd06d3e68690f2040
nicholettesams <nicholettesams@msn.com>
2018-09-08 17:02:16 -0400

    Trying to get just click event to fire

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,5 @@
 // Initialize Firebase
+/*
   var config = {
     apiKey: "AIzaSyCQcVj-0Be5_riZGy78furc9QI1gTl2vno",
     authDomain: "train-scheduler-b6603.firebaseapp.com",
@@ -9,3 +10,44 @@
   };
   
   firebase.initializeApp(config);
+  var database = firebase.database();
+*/
+  // Whenever a user clicks the submit-train button
+$("#submit-train").on("click", function(event) {
+  console.log("clicked!")
+  // Prevent form from submitting
+  event.preventDefault();
+
+  // Get the input values
+  var trainName = $("#train-input").val()
+  var destination = $("#destination-input").val()
+  var firstTime = $("#time-input").val()
+  var frequency = $("#freq-input").val()
+
+  //log for debugging
+  console.log(trainName)
+  console.log(destination)
+  console.log(firstTime)
+  console.log(frequency)
+
+    // Save the new price in Firebase
+  /*
+    database.ref('trains').set({
+      trainName: trainName,
+      destination: destination,
+      firstTime: firstTime,
+      frequency: frequency
+    });
+*/
+
+    // Change the HTML to reflect the new train
+    var newTR = $("<tr>")
+    var newTDName = $("<td>").text(trainName)
+    var vewTDDest = $("<td>").text(destination)
+    var newTDTime = $("<td>").text(firstTime)
+    var newTDMin = $("<td>").text(frequency)
+
+    newTR.append(newTDName).append(vewTDDest).append(newTDTime).append(newTDMin)
+    $("#train-table-body").append(newTR)
+
+});
