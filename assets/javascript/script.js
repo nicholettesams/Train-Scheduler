ed4b7016410e405a019649705fbfd1758dc3a75f
nicholettesams <nicholettesams@msn.com>
2018-09-08 13:40:32 -0400

    firebase config

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCQcVj-0Be5_riZGy78furc9QI1gTl2vno",
+    authDomain: "train-scheduler-b6603.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b6603.firebaseio.com",
+    projectId: "train-scheduler-b6603",
+    storageBucket: "train-scheduler-b6603.appspot.com",
+    messagingSenderId: "995481655505"
+  };
+  
+  firebase.initializeApp(config);
